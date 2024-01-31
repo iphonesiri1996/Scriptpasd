3acb32b61020155fa01a3aa8fc486abf2ca5fb23
iphonesiri1996 <navjyotabhale0105@gmail.com>
2024-01-31 16:40:22 +0530

    add ai file

---
+++ ai.py
@@ -0,0 +1,9 @@
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
